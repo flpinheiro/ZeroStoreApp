eb56400e21aa68a388e79e9e81cc8bf1caee1440
Felipe Pinheiro <flpinheiro@gmail.com>
2025-03-06 13:33:09 -0300

    update frontend

---
+++ ZeroStoreApp.Web/Models/ProductModel.cs
@@ -0,0 +1,40 @@
+﻿namespace ZeroStoreApp.Web.Models;
+
+public class ProductModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+}
+
+public class ProductListModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+}
+
+public class CreateProductModel
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
+
+public class UpdateProductModel 
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
+
+public class ProductRequest 
+{
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? Query { get; set; }
+}
